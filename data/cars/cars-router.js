25a39c9f1d674ac56a3e8e658b1b437e697e0f91
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-12-10 23:43:56 -0600

    POST request working

--- data/cars/cars-router.js
+++ data/cars/cars-router.js
@@ -18,6 +18,32 @@ router.get('/', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    const carData = req.body;
+    if(carData.vin && carData.make && carData.model && carData.mileage) {
+        db("cars")
+            .insert(carData, 'id')
+            .then(ids => {
+                const id = ids[0]
+
+                return db("cars")
+                    .select('id','vin','make','model','mileage')
+                    .where({ id })
+                    .first()
+                    .then(car => {
+                        res.status(201).json(car)
+                    })
+            })
+            .catch(error => {
+                res.status(500)
+                    .json({ error: "Error adding new car to database" })
+            })
+    } else {
+        res.status(400)
+            .json({ message: "Please include all fields" })
+    }
+})
+
 
 
 

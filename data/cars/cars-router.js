1c6f73f9f0a0b9bfef51fb498d50fcda71da9e38
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-12-10 23:34:29 -0600

    GET request working

--- data/cars/cars-router.js
+++ data/cars/cars-router.js
@@ -1,8 +1,22 @@
 const express = require("express");
 
+const db = require('../dbConfig')
+
 const router = express.Router();
+router.use(express.json());
 
-
+router.get('/', (req, res) => {
+    db
+        .select("*")
+        .from("cars")
+        .then(cars => {
+            res.status(200).json(cars)
+        })
+        .catch(error => {
+            res.status(500)
+                .json({ error: "Error getting cars" })
+        })
+})
 
 
 
